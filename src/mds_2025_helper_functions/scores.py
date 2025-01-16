3d3b4bd772cd1bd507e223859ab568e5e9922327
mdah <mda.hewlett@gmail.com>
2025-01-16 11:48:58 -0800

    feat: add functional code to compare_model_scores()

--- src/mds_2025_helper_functions/scores.py
+++ src/mds_2025_helper_functions/scores.py
@@ -1,3 +1,7 @@
+from sklearn.model_selection import cross_validate
+import numpy as np
+import pandas as pd
+
 def compare_model_scores(*args, X, y=None, scoring=None, return_train_scores=False, **kwargs):
     """Creates a table comparing mean CV scores of multiple models.
     Parameters
@@ -29,4 +33,19 @@ def compare_model_scores(*args, X, y=None, scoring=None, return_train_scores=Fal
         - Columns: Score metrics
         - Index: Model names
     """
-    return
+    results = []
+    for model in args:
+        cv_results = cross_validate(
+            model,
+            X=X,
+            y=y,
+            scoring=scoring,
+            return_train_score=return_train_scores,
+            **kwargs
+        )
+
+        mean_scores = {key: np.mean(val) for key, val in cv_results.items()}
+        mean_scores['model'] = model.__class__.__name__
+        results.append(mean_scores)
+
+    return pd.Dataframe(results).set_index('model')
