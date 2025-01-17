efd57b24bc8eb3fc1408aca2499072e79f7efbd2
mdah <mda.hewlett@gmail.com>
2025-01-16 16:44:23 -0800

    docs: rename variables for readability

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -7,32 +7,30 @@ import numpy as np
 from unittest.mock import patch
 
 # Mock data
-x = pd.DataFrame(
+mock_features = pd.DataFrame(
     {
         "feature_numeric": [1, 2, 3, 10],
         "feature_categorical": ["apple", "banana", "orange", "apple"]
     }
 )
-y_regression = pd.DataFrame(
+mock_target_regression = pd.DataFrame(
     {
         "target_numeric": [5, 10, 14, 2]
     }
 )
-y_classification = pd.DataFrame(
+
+mock_target_classification = pd.DataFrame(
     {
         "target_categorical": ["olive", "cypress", "cypress", "oak"]
     }
 )
+
 dummy_r = DummyRegressor()
 tree_r = DecisionTreeRegressor()
 tree_r2 = DecisionTreeRegressor(max_depth=2)
 dummy_c = DummyClassifier()
 tree_c = DecisionTreeClassifier()
-non_sklearn_object = []
-
-output_col_names = ["fit_time", "score_time", "test_score", "train_score"]
-output_index_names_regression = ["DummyRegressor", "DecisionTreeRegressor"]
-output_index_names_classification = ["DummyClassifier", "DecisionTreeClassifier"]
+non_sklearn_model = []
 
 mock_cv_results = {
     'test_score': np.array([0.7, 0.8, 0.9]),
@@ -41,11 +39,16 @@ mock_cv_results = {
     'score_time': np.array([0.05, 0.05, 0.05])
 }
 
+output_col_names = ["fit_time", "score_time", "test_score", "train_score"]
+output_index_names_regression = ["DummyRegressor", "DecisionTreeRegressor"]
+output_index_names_classification = ["DummyClassifier", "DecisionTreeClassifier"]
+
+
 # Success tests
 def test_compare_model_scores_regression():
     """For regression models, returns a pandas dataframe with the metrics as column names and models as index names"""
     with patch('mds_2025_helper_functions.scores.cross_validate', return_value = mock_cv_results):
-        result_r = compare_model_scores(dummy_r, tree_r, X=x, y=y_regression, return_train_scores=True)
+        result_r = compare_model_scores(dummy_r, tree_r, X=mock_features, y=mock_target_regression, return_train_scores=True)
         result_r_index = result_r.index.tolist()
         result_r_colnames = result_r.columns.tolist()
         
@@ -58,7 +61,7 @@ def test_compare_model_scores_regression():
 def test_compare_model_scores_classification():
     """For classification models, returns a pandas dataframe with the metrics as column names and models as index names"""
     with patch('mds_2025_helper_functions.scores.cross_validate', return_value = mock_cv_results):
-        result_c = compare_model_scores(dummy_c, tree_c, X=x, y=y_classification, return_train_scores=True)
+        result_c = compare_model_scores(dummy_c, tree_c, X=mock_features, y=mock_target_classification, return_train_scores=True)
         result_c_index = result_c.index.tolist()
         result_c_colnames = result_c.columns.tolist()
 
@@ -72,7 +75,7 @@ def test_compare_model_scores_classification():
 def test_compare_model_scores_same_model_type():
     """Function handles multiple instances of same model type"""
     with patch('mds_2025_helper_functions.scores.cross_validate', return_value = mock_cv_results):
-        result = compare_model_scores(tree_r, tree_r2, X=x, y=y_regression, return_train_scores=True)
+        result = compare_model_scores(tree_r, tree_r2, X=mock_features, y=mock_target_regression, return_train_scores=True)
     
     assert result.index[0] != result.index[1]
 
@@ -82,7 +85,7 @@ def test_compare_model_scores_single_model():
     expected_error = "compare_model_scores() requires at least 2 models. You provided 1."
     
     with pytest.raises(TypeError) as e:
-        compare_model_scores(dummy_r, X=x, y=y_regression, return_train_scores=True)
+        compare_model_scores(dummy_r, X=mock_features, y=mock_target_regression, return_train_scores=True)
     
     actual_error = str(e.value)
     assert actual_error == expected_error
@@ -92,7 +95,7 @@ def test_compare_model_scores_invalid_model():
     expected_error = "All models must be sklearn models. The following argument is not an sklearn model: []"
 
     with pytest.raises(TypeError) as e:
-        compare_model_scores(dummy_r, non_sklearn_object, X=x, y=y_regression, return_train_scores=True)
+        compare_model_scores(dummy_r, non_sklearn_model, X=mock_features, y=mock_target_regression, return_train_scores=True)
         
     actual_error = str(e.value)
     assert actual_error == expected_error
@@ -102,7 +105,7 @@ def test_compare_model_scores_mixed_models():
     expected_error = "All models must be of the same type. Found multiple types: classifier, regressor"
     
     with pytest.raises(ValueError) as e:
-        compare_model_scores(dummy_r, dummy_c, X=x, y=y_regression)
+        compare_model_scores(dummy_r, dummy_c, X=mock_features, y=mock_target_regression)
 
     actual_error = str(e.value)
     assert actual_error == expected_error
